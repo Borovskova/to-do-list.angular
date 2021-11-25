42309f4d03955ea822974fb7aeaa4530200ea63f
Borovskova <belka.sixa@gmail.com>
2021-11-25 21:41:49 +0300

    'f-c'

--- src/app/services/tasks-data.service.ts
+++ src/app/services/tasks-data.service.ts
@@ -5,22 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class TasksDataService {
 
-  public list:any = [];
-  public newTask:any;
-  public listFromLocalStorage:any;
+  public list: any = [];
+  public newTask: any;
+  public listFromLocalStorage: any;
 
   constructor() { }
 
   public removeTask(task: any) {
     let taskForRemove = this.list.findIndex((item: any) => item.id === task.id);
     this.list.splice(taskForRemove, 1)
-    // if (this.listFromLocalStorage){
-    //   this.listFromLocalStorage.splice(taskInLocalStorForRemove, 1)
-    //   this.list = this.list.concat(JSON.parse(this.listFromLocalStorage));
-    //   console.log('list' + this.list[0]);
-    // }
-    this.newTask.id =  this.list.indexOf(this.newTask);
+    this.newTask.id = this.list.indexOf(this.newTask);
     localStorage.setItem('list', JSON.stringify(this.list));
-    console.log(this.list); 
   }
 }
